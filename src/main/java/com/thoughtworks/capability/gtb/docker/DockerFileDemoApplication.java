b8fdfabdde918a9cfb2e1354f6aa3c9e81102d56
ZhaoZhang <zhaozhang@thoughtworks.com>
2020-10-25 22:00:32 +0800

    demo docker file start

--- src/main/java/com/thoughtworks/capability/gtb/docker/DockerFileDemoApplication.java
+++ src/main/java/com/thoughtworks/capability/gtb/docker/DockerFileDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringDockerDemoApplication {
+public class DockerFileDemoApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(SpringDockerDemoApplication.class, args);
+    SpringApplication.run(DockerFileDemoApplication.class, args);
   }
 
 }
